4cf86443f2edfeeb6cb96b9952ad4f06920a9d44
Galaxy-Coding <elias.salmi@icloud.com>
2020-12-11 18:46:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);

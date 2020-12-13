ffa63696f0397fba72e6a78a47a94509d6a3f5f0
Galaxy-Coding <elias.salmi@icloud.com>
2020-12-13 19:51:43 +0000

    new commands

--- index.js
+++ index.js
@@ -12,7 +12,33 @@ client.on('message', message => {
 	if (message.content === `${prefix} ping`) {
 		message.channel.send('pong');
 	} else if (message.content === `${prefix} beep`) {
-		message.channel.send('boop');
+		message.channel.send({
+			embed: {
+				description: `Info about <@${message.author.id}>`,
+				color: 123456,
+				timestamp: message.createdTimestamp,
+				thumbnail: {
+					url: message.author.displayAvatarURL()
+				},
+				author: {
+					name: message.author.username,
+					icon_url: message.author.displayAvatarURL()
+				},
+				fields: [
+					{
+						name: 'Joined at:',
+						timestamp: message.author.joinedAt,
+						inline: true
+					},
+
+					{
+						name: '<:thonkang:219069250692841473>',
+						value: 'are inline fields',
+						inline: true
+					}
+				]
+			}
+		});
 	} else if (message.content === `${prefix} server`) {
 		message.channel.send(
 			`Server name: ${message.guild.name}\nTotal members: ${
@@ -29,6 +55,7 @@ client.on('message', message => {
 		message.channel.send(
 			`Commands:
 **.d help**
+**.d invite**
 .d ping
 .d beep
 .d server
@@ -52,7 +79,14 @@ client.on('message', message => {
 .d panda fact
 .d cat fact
 .d dog fact
-.d trump`
+.d trump
+.d clear
+.d geek joke
+.d ron swanson
+.d trigger avatar
+.d kanye west
+.d chuck norris
+.d star wars`
 		);
 	} else if (message.content == `${prefix} lag`) {
 		message.channel.send(
@@ -116,10 +150,10 @@ client.on('message', message => {
 				message.channel.send(json.fact);
 			});
 	} else if (message.content == `${prefix} dog pic`) {
-		fetch('https://some-random-api.ml/img/dog', settings)
+		fetch('https://dog.ceo/api/breeds/image/random', settings)
 			.then(res => res.json())
 			.then(json => {
-				message.channel.send(json.link);
+				message.channel.send(json.message);
 			});
 	} else if (message.content == `${prefix} cat pic`) {
 		fetch('https://some-random-api.ml/img/cat', settings)
@@ -165,7 +199,60 @@ client.on('message', message => {
 			.then(json => {
 				message.reply(`\n> ${json.value}\n-Donald Trump`);
 			});
+	} else if (message.content == `${prefix} invite`) {
+		message.reply(
+			`Join the bot here: https://discord.com/oauth2/authorize?client_id=787006555761279006&scope=bot&permissions=8`
+		);
+	} else if (message.content === `${prefix} trigger avatar`) {
+		message.channel.send(
+			`https://some-random-api.ml/canvas/triggered?avatar=${encodeURIComponent(
+				message.author.displayAvatarURL().replace('webp', 'png')
+			)}`
+		);
+	} else if (message.content === `${prefix} clear`) {
+		if (message.member.hasPermission('MANAGE_MESSAGES')) {
+			message.channel.messages.fetch().then(results => {
+				message.channel.bulkDelete(results);
+			});
+		}
+	} else if (message.content == `${prefix} kanye west`) {
+		fetch('https://api.kanye.rest/?format=json', settings)
+			.then(res => res.json())
+			.then(json => {
+				message.channel.send(json.quote);
+			});
+	} else if (message.content == `${prefix} ron swanson`) {
+		fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes', settings)
+			.then(res => res.json())
+			.then(json => {
+				message.channel.send(json[0]);
+			});
+	} else if (message.content == `${prefix} geek joke`) {
+		fetch('https://geek-jokes.sameerkumar.website/api?format=json', settings)
+			.then(res => res.json())
+			.then(json => {
+				message.channel.send(json.joke);
+			});
+	} else if (message.content == `${prefix} chuck norris`) {
+		fetch('https://api.chucknorris.io/jokes/random', settings)
+			.then(res => res.json())
+			.then(json => {
+				message.channel.send(json.value);
+			});
+	} else if (message.content == `${prefix} star wars`) {
+		fetch('http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote', settings)
+			.then(res => res.json())
+			.then(json => {
+				message.channel.send(json.starWarsQuote);
+			});
+	} else if (message.content == `${prefix} star wars`) {
+		fetch('http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote', settings)
+			.then(res => res.json())
+			.then(json => {
+				message.channel.send(json.starWarsQuote);
+			});
 	}
 });
-
 client.login(process.env.DISCORD_TOKEN);
+
+//https://api.quotable.io/random
